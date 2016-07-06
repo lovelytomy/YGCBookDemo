ea741b3cffc087aa1dff4baff3f7808632a5cce3
lovelytomy <535303865@qq.com>
2016-07-06 15:58:35 +0800

    add a test

---
+++ GuideTest/GuideTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuideTest
+//
+//  Created by tianqi on 16/7/4.
+//  Copyright © 2016年 com.wiscess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GuideTest/GuideTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GuideTest
+//
+//  Created by tianqi on 16/7/4.
+//  Copyright © 2016年 com.wiscess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
