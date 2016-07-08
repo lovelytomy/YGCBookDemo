4d0a268af8bff8cfa795bfe1e1f0b66c9ec4cd20
lovelytomy <535303865@qq.com>
2016-07-08 13:21:04 +0800

    add 7 demo

---
+++ GuideTest/GuideTest/SevenViewController.h
@@ -0,0 +1,13 @@
+//
+//  SevenViewController.h
+//  GuideTest
+//
+//  Created by tianqi on 16/7/8.
+//  Copyright © 2016年 com.wiscess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SevenViewController : UIViewController
+
+@end
